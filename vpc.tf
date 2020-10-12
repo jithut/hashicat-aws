7cd19ca111a6e1bfe9ca8d65e1310034f9ffa1f5
Jithu <jithusreyas@gmail.com>
2020-10-12 22:55:10 +0000

    Added VPC module

--- vpc.tf
+++ vpc.tf
@@ -1,7 +1,6 @@
 module "vpc" {
   source  = "app.terraform.io/jithut-training/vpc/aws"
   version = "2.57.0"
-  cidr_block           = "10.0.0.0/16"
   enable_dns_hostnames = true
   # insert required variables here
 }
